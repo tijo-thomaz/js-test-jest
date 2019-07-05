d2eea4fdb530fb260025f6ea2b7e5d27df7e3173
Tijo <tijo@QB-TP3-1605.local>
2019-07-05 17:50:05 +0530

    e2e updated

--- util.test.js
+++ util.test.js
@@ -16,17 +16,21 @@ test('generate a valid text',()=>{
 });
 
 //e2e
-test('should click around', async()=>{
-    const browser= await puppeteer.launch({
-        headless:false,
-        slowMo:80,
-        args:['--window-size=700,1080']
+test('should create an element with text and correct class', async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      slowMo: 80,
+      args: ['--window-size=1920,1080']
     });
-const page = await browser.newPage();
-await page.goto(`file:///Users/tijo/Documents/js-testing-introduction/index.html`);
-await page.click('input#name');
-await page.type('input#name','Tijo')
-await page.click('input#age',);
-await page.type('input#name','Tijo')
-await page.click('#btnAddUser');
-});
+    const page = await browser.newPage();
+    await page.goto(
+      'file:///Users/tijo/Documents/js-testing-introduction/index.html'
+    );
+    await page.click('input#name');
+    await page.type('input#name', 'Tijo');
+    await page.click('input#age');
+    await page.type('input#age', '28');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Tijo (28 years old)');
+  }, 10000);
